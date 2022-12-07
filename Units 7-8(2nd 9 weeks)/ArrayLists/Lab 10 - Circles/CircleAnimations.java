1540e7c8404e621583f2cc4c37b15d8e793e5bc2
SXV357 <shrevis2018@gmail.com>
2022-12-07 11:16:56 -0600

    cleanup

--- Units 7-8(2nd 9 weeks)/ArrayLists/Lab 10 - Circles/CircleAnimations.java
+++ Units 7-8(2nd 9 weeks)/ArrayLists/Lab 10 - Circles/CircleAnimations.java
@@ -3,19 +3,17 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class CircleAnimations {
-	private ArrayList<Circle> circles; // the circles to animate
-	private int size; // canvas width and height (will be square)
-	private Random rng; // use to make random numbers
+	private ArrayList<Circle> circles;
+	private int size;
+	private Random rng;
 
-	/** create a drawing pane of a particular size */
 	public CircleAnimations(int s) {
 		circles = new ArrayList<>();
 		size = s;
 		rng = new Random();
 
-		// don't mess with this
-		StdDraw.setCanvasSize(size, size); // set up drawing canvas
-		StdDraw.setXscale(0, size); // <0, 0> is bottom left. <size-1, size-1> is top right
+		StdDraw.setCanvasSize(size, size);
+		StdDraw.setXscale(0, size);
 		StdDraw.setYscale(0, size);
 	}
 
--- Units 7-8(2nd 9 weeks)/ArrayLists/Lab 10 - Circles/CircleRunner.java
+++ Units 7-8(2nd 9 weeks)/ArrayLists/Lab 10 - Circles/CircleRunner.java
@@ -2,27 +2,17 @@ import java.awt.*;
 
 public class CircleRunner {
 	public static void main(String[] args) {
-		CircleAnimations app = new CircleAnimations(600); //supply window size, will be 600x600
-
-		//test your methods below (with the app object)
-		// testCircleOverlap();
+		CircleAnimations app = new CircleAnimations(600);
 		app.removeClicked();
 
-
-//		testCircleOverlap(); //uncomment to test your overlap method, when required
 	}
 
-	public static void testCircleOverlap() { //uncomment contents to use
-		/*
-		 * The distance between a and b is ~2.83, which is greater than the combined radius of 2 (a and b do NOT overlap)
-		 *
-		 * The distance between a and c is ~2.83, which is less    than the combined radius of 6 (a and c overlap)
-		 */
-		Circle a = new Circle(1, 1, 1, null); //color is irrelevant for this
+	public static void testCircleOverlap() {
+		Circle a = new Circle(1, 1, 1, null);
 		Circle b = new Circle(3, 3, 1, null);
 		Circle c = new Circle(3, 3, 5, null);
 
-		System.out.println(a.overlaps(b)); //should print false
-		System.out.println(a.overlaps(c)); //should print true
+		System.out.println(a.overlaps(b));
+		System.out.println(a.overlaps(c));
 	}
 }
