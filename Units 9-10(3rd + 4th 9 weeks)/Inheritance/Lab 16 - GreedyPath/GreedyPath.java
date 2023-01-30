e7245bf5e8fc72a05fed80d7d8a15f2006b1442d
SXV357 <shrevis2018@gmail.com>
2023-01-29 18:42:20 -0600

    greedypath class comments for explaining logic

--- Units 9-10(3rd + 4th 9 weeks)/Inheritance/Lab 16 - GreedyPath/GreedyPath.java
+++ Units 9-10(3rd + 4th 9 weeks)/Inheritance/Lab 16 - GreedyPath/GreedyPath.java
@@ -8,32 +8,34 @@ public class GreedyPath extends Path {
 
   public GreedyPath(String fileName) throws FileNotFoundException {
     super(fileName);
-    this.greedyPoints = new Point[0];
+    this.greedyPoints = new Point[0]; // initializing local array so it doesn't throw a null exception
     findPath();
   }
 
   private void findPath() {
     ArrayList<Point> path = new ArrayList<Point>();
     Point start = super.getPoint(0);
+    // always starting from very first point in the array before moving on to other ones
     if (start != null) {
       start.setVisited(true);
       path.add(start);
     }
 
-    while (path.size() < super.getPoints().length) {
-      if (path.size() > 0) {
+    while (path.size() < super.getPoints().length) { // checking every single point in the points array contained in Point class
+      if (path.size() > 0) { // ensure there's at least one point in the arraylist to start cgecking
         Point current = path.get(path.size() - 1);
-        Point closest = super.getPoint(0);
+        Point closest = super.getPoint(0); // assuming that the very first one is the closest to start off with
         double minDistance = Double.MAX_VALUE;
         for (int i = 0; i < super.getPoints().length; i++) {
           if (!super.getPoints()[i].getVisited()) {
-            double distance = current.getDistance(getPoints()[i]);
+            double distance = current.getDistance(super.getPoints()[i]); // checking if a point is not visited and then getting the distance between the current point being traversed and that specific point
             if (distance < minDistance) {
-              minDistance = distance;
+              minDistance = distance; // logic for updating min distance as necessary
               closest = super.getPoints()[i];
             }
           }
         }
+        // core logic to update the greedy points array and the total distance covered
         if (closest != null) {
           closest.setVisited(true);
           path.add(closest);
@@ -42,6 +44,7 @@ public class GreedyPath extends Path {
       }
 
       if (path.size() > 0) {
+        // making the contents of Path more accessible as long as it contains Point objects that are not that far from the current point being traversed in the original array
         this.greedyPoints = path.toArray(new Point[0]);
       }
     }
