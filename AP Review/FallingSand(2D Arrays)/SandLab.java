7149cc932ddff8e1431270bbca8cb86f366c3d85
SXV357 <shrevis2018@gmail.com>
2023-04-09 13:27:02 -0500

    sandlab ap review

---
+++ AP Review/FallingSand(2D Arrays)/SandLab.java
@@ -0,0 +1,94 @@
+import java.awt.*;
+
+public class SandLab {
+
+  //add constants for particle types here
+  public static final int EMPTY = 0;
+  public static final int METAL = 1;
+  public static final int SAND = 2;
+  public static final int WATER = 3;
+
+  //do not add any more fields!
+  private int[][] grid;
+  private SandDisplay display; //SandDisplay is the GUI class
+
+  public SandLab(int numRows, int numCols) {
+    String[] names = new String[4];
+
+    names[EMPTY] = "Empty";
+    names[METAL] = "Metal";
+    names[SAND] = "Sand";
+    names[WATER] = "Water";
+
+    display = new SandDisplay("Falling Sand", numRows, numCols, names);
+    grid = new int[numRows][numCols];
+  }
+
+  /** called when the user clicks on a location using the given tool */
+  private void locationClicked(int row, int col, int tool) {
+    grid[row][col] = tool;
+  }
+
+  /** copies each element of grid into the display */
+  public void updateDisplay() {
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        switch (grid[i][j]) {
+          case EMPTY:
+            display.setColor(i, j, new Color(0, 0, 0));
+            break;
+          case METAL:
+            display.setColor(i, j, new Color(163, 164, 159));
+            break;
+          case SAND:
+            display.setColor(i, j, new Color(253, 218, 13));
+            break;
+          case WATER:
+            display.setColor(i, j, new Color(0, 0, 255));
+            break;
+        }
+      }
+    }
+  }
+
+  /** called repeatedly, causes one random particle to maybe do something */
+  public void step() {
+    int randomRow = (int) Math.floor(Math.random() * grid.length);
+    int randomCol = (int) Math.floor(Math.random() * grid[0].length);
+    if (
+      randomRow >= 0 &&
+      randomRow <= grid.length - 1 &&
+      randomCol >= 0 &&
+      randomCol <= grid[0].length - 1
+    ) {
+      int gridVal = grid[randomRow][randomCol];
+      if (!(gridVal == METAL) && randomRow != grid.length - 1) {
+        if (gridVal == SAND && grid[randomRow + 1][randomCol] == EMPTY) {
+          grid[randomRow + 1][randomCol] = SAND;
+          grid[randomRow][randomCol] = EMPTY;
+        }
+      }
+    }
+  }
+
+  //do not modify!
+  public void run() {
+    while (true) {
+      for (int i = 0; i < display.getSpeed(); i++) step();
+
+      updateDisplay();
+
+      display.repaint();
+
+      display.pause(1); //wait for redrawing and for mouse
+
+      int[] mouseLoc = display.getMouseLocation();
+
+      if (mouseLoc != null) locationClicked(
+        mouseLoc[0],
+        mouseLoc[1],
+        display.getTool()
+      ); //test if mouse clicked
+    }
+  }
+}
